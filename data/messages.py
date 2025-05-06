9878ef6f4026da18c743b1d0129bd169dde7a3e4
Harry <wheeler_harry@icloud.com>
2025-05-06 18:06:00 +0100

    Create messages.py

---
+++ data/messages.py
@@ -0,0 +1,3 @@
+online="""Hello! I'm currently online. If you need anything, feel free to ask!"""
+idle="""Hello! I'm currently idle. I might not respond immediately, but I'll get back to you as soon as I can!"""
+dnd="""Hello! I'm currently do not disturb. I might not respond immediately, but I'll get back to you as soon as I can!"""
